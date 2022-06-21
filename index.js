e1e132f915ac1e7c5db5f80b8e1430b60550e7ae
LIZXP <Peter.xli@outlook.com>
2022-06-21 10:33:27 -0600

    finish the 3rd excersise

---
+++ index.js
@@ -0,0 +1,30 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log(ip);
+});
+
+fetchCoordsByIP("205.206.237.222", (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned coordinates:", coordinates);
+});
+
+const exampleCoords = { latitude: "49.27670", longitude: "-123.13000" };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned flyover times:", passTimes);
+});
